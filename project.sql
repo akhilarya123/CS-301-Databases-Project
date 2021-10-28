8b7f0a7ef75936b862fd15ec790623dd91b30140
Aditya Agarwal <2019csb1064@iitrpr.ac.in>
2021-10-29 01:53:31 +0530

    Update project.sql

--- project.sql
+++ project.sql
@@ -44,16 +44,22 @@ CREATE TABLE student_record(
     department varchar(3) not null
 );
 
-CREATE TABLE current_info(
-    holder varchar(4) not null,
-    sem integer not null,
-    yr integer not null
-);
-
 CREATE TABLE instructor_record(
     teacher_id varchar(12) primary key,
     teacher_name varchar(25) not null,
     department varchar(3) not null
+);
+
+CREATE TABLE batch_advisor_record(
+    ba_id varchar(12) primary key,
+    ba_name varchar(25) not null,
+    department varchar(3) not null
+);
+
+CREATE TABLE current_info(
+    holder varchar(4) not null,
+    sem integer not null,
+    yr integer not null
 );
 
 INSERT INTO current_info values('curr', 1, 2021);
@@ -108,7 +114,8 @@ FOR EACH ROW
 EXECUTE PROCEDURE _insert_course_offerings();
 
 -------------------------------------------------------------------------------------
-CREATE OR REPLACE FUNCTION enrol(course_id varchar(6))
+
+CREATE OR REPLACE FUNCTION generate_ticket(course_id varchar(6), section_id integer)
 RETURNS void
 LANGUAGE PLPGSQL
 AS $$
@@ -118,7 +125,23 @@ cred real;
 BEGIN
 EXECUTE FORMAT('SELECT * from current_info c where c.holder = ''curr'';') into curr;
 EXECUTE FORMAT('SELECT c.credits from course_catalogue c where c.course_id = course_id;') into cred;
-EXECUTE FORMAT('INSERT INTO %I values(%L, %L, %L, %L);', current_user||'_enr', course_id, curr.sem, curr.yr, cred);
+EXECUTE FORMAT('INSERT INTO %I values(%L, %L, %L, %L);', current_user||'_enr', course_id, section_id, curr.sem, curr.yr, now(), "Pending Instructor Approval");
+END;
+$$;
+
+-------------------------------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION enrol(course_id varchar(6), section_id integer)
+RETURNS void
+LANGUAGE PLPGSQL
+AS $$
+DECLARE
+curr record;
+cred real;
+BEGIN
+EXECUTE FORMAT('SELECT * from current_info c where c.holder = ''curr'';') into curr;
+EXECUTE FORMAT('SELECT c.credits from course_catalogue c where c.course_id = course_id;') into cred;
+EXECUTE FORMAT('INSERT INTO %I values(%L, %L, %L, %L);', current_user||'_enr', course_id, section_id, curr.sem, curr.yr, cred);
 END;
 $$;
 
@@ -190,7 +213,7 @@ END;
 $$;
 
 -------------------------------------------------------------------------------------
-CREATE OR REPLACE FUNCTION _check_enrol()
+CREATE OR REPLACE FUNCTION _student_enr_before()
 RETURNS TRIGGER
 LANGUAGE PLPGSQL
 AS $$
@@ -273,6 +296,122 @@ END;
 $$;
 
 -------------------------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION _student_enr_after()
+RETURNS TRIGGER
+LANGUAGE PLPGSQL SECURITY DEFINER
+AS $$
+DECLARE
+curr record;
+
+BEGIN
+EXECUTE FORMAT('SELECT * from current_info c where c.holder = ''curr'';') into curr;
+EXECUTE FORMAT('INSERT INTO %I VALUES(%L);', NEW.course_id||'_'||NEW.course_id||'_students', session_user)
+
+RETURN NEW;
+END;
+$$;
+
+-------------------------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION _student_ticket_before()
+RETURNS TRIGGER
+LANGUAGE PLPGSQL
+AS $$
+DECLARE
+curr record;
+cgpa real;
+criteria real;
+cred real;
+curr_cred real;
+flag integer;
+r record;
+s record;
+t record;
+prereq varchar(6);
+BEGIN
+EXECUTE FORMAT('SELECT * from current_info c where c.holder = ''curr'';') into curr;
+IF NEW.course_id NOT IN (select course_offerings.course_id from course_offerings) THEN
+RAISE EXCEPTION 'Course does not exist!';
+END IF;
+
+IF NEW.section_id NOT IN (select course_offerings.section_id from course_offerings) THEN
+RAISE EXCEPTION 'Section does not exist!';
+END IF;
+
+IF NEW.sem != curr.sem THEN
+RAISE EXCEPTION 'Incorrect Semester!';
+END IF;
+
+IF NEW.yr != curr.yr THEN
+RAISE EXCEPTION 'Incorrect Year!';
+END IF;
+
+criteria := 11;
+SELECT get_cgpa() into cgpa;
+SELECT cgpa_criteria FROM course_offerings where NEW.course_id = course_offerings.course_id into criteria;
+IF cgpa<criteria and criteria<11 THEN
+RAISE EXCEPTION 'CGPA too low!';
+END IF;
+
+flag := 0;
+SELECT * from student_record where current_user = student_record.student_id into s;
+for r in EXECUTE FORMAT('SELECT * FROM batch_req where NEW.course_id = batch_req.course_id;') loop
+IF r.yr = s.yr and r.department = s.department THEN
+flag := 1;
+END IF;
+END LOOP;
+IF flag = 0 THEN
+RAISE EXCEPTION 'Your batch is ineligible for this course!';
+END IF;
+
+r:=row(null);
+FOR prereq in EXECUTE FORMAT('SELECT prereq from prerequisite where NEW.course_id = prerequisite.course_id;') loop
+EXECUTE FORMAT('SELECT * FROM %I where prereq = %I.course_id;', current_user||'_tt', current_user||'_tt') INTO r;
+IF r is null THEN
+RAISE EXCEPTION 'Prerequisites not matched!';
+END IF;
+END loop;
+
+FOR r IN (SELECT * FROM time_table where NEW.course_id = time_table.course_id) loop
+FOR s IN EXECUTE FORMAT('SELECT * FROM %I;', current_user||'_enr') loop
+FOR t IN (SELECT * FROM time_table where s.course_id = time_table.course_id) loop
+IF r.day_of_week = t.day_of_week and r.slot = t.slot THEN
+RAISE EXCEPTION 'Time slot clash with %', s.course_id;
+END IF;
+END loop;
+END loop;
+END loop;
+
+SELECT get_prev_creds() into cred;
+EXECUTE FORMAT('SELECT SUM(credits) from %I;', current_user||'_enr') into curr_cred;
+IF curr_cred + NEW.credits <= 1.25*cred THEN
+RAISE EXCEPTION 'Within credit limit! Enrol normally.';
+END IF;
+RETURN NEW;
+END;
+$$;
+
+-------------------------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION _student_ticket_after()
+RETURNS TRIGGER
+LANGUAGE PLPGSQL SECURITY DEFINER
+AS $$
+DECLARE
+curr record;
+tid varchar(12);
+BEGIN
+EXECUTE FORMAT('SELECT * from current_info c where c.holder = ''curr'';') into curr;
+SELECT * FROM course_offerings where NEW.course_id = course_offerings.course_id and NEW.section_id = course_offerings.section_id into tid;
+EXECUTE FORMAT('INSERT INTO %I VALUES(%L, %L, %L, %L, %L, %L);', tid||'_ticket', session_user, NEW.course_id, NEW.section_id, curr.sem, curr.yr, "Pending Instructor Approval");
+
+RETURN NEW;
+END;
+$$;
+
+-------------------------------------------------------------------------------
+
 CREATE OR REPLACE FUNCTION new_student(student_id varchar(12), student_name varchar(25), yr integer, department varchar(3))
 RETURNS void
 LANGUAGE PLPGSQL
@@ -291,6 +430,7 @@ EXECUTE FORMAT('INSERT INTO student_record values(%L, %L, %L, %L);', student_id,
 --Enrollment table
 EXECUTE FORMAT('CREATE TABLE %I(
     course_id varchar(6),
+    section_id integer not null,
     sem integer not null,
     yr integer not null,
     credits real not null,
@@ -326,12 +466,19 @@ EXECUTE FORMAT('GRANT SELECT on %I to %I;', student_id||'_tt', student_id);
 
 EXECUTE FORMAT('GRANT SELECT on %I to %I;', student_id||'_ticket', student_id);
 EXECUTE FORMAT('GRANT INSERT on %I to %I;', student_id||'_ticket', student_id);
+EXECUTE FORMAT('GRANT STD to %I;', student_id);
 
 EXECUTE FORMAT('CREATE TRIGGER %I
 BEFORE INSERT
 ON %I
 FOR EACH ROW
-EXECUTE PROCEDURE _check_enrol();', student_id||'_trig', student_id||'_enr');
+EXECUTE PROCEDURE _student_enr_before();', student_id||'_enr_before', student_id||'_enr');
+
+EXECUTE FORMAT('CREATE TRIGGER %I
+AFTER INSERT
+ON %I
+FOR EACH ROW
+EXECUTE PROCEDURE _student_enr_after();', student_id||'_enr_before', student_id||'_enr');
 
 END;
 $$;
