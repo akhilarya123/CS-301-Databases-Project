6f0ee6db471672a249db5b4cb57cbe6a84067ced
Akhil Arya <akhil.cse.ropar@gmail.com>
2021-11-01 17:31:12 +0530

    Update initialise.sql

--- initialise.sql
+++ initialise.sql
@@ -6,4 +6,18 @@
  select new_student('csb2','adi',2019,'cs');
  select new_student('csb3','rahul',2019,'mec');
  select new_student('csb4','rohan',2019,'mec');
+ select new_student('csb5','raghav',2019,'ec');
+ select new_student('csb6','raghav',2019,'ec');
+ select new_instructor('ins1','gunturi','cs');
+ select new_instructor('ins2','hansan','cs');
+ select new_instructor('ins3','dewan','cs');
+ select new_instructor('ins4','mathpal','cs');
+ select new_instructor('ins5','sarit','mec');
+ select new_instructor('ins6','robert','cs');
+ INSERT INTO course_offerings VALUES ('cs301',1,2019,'ins1',1,0);
+ 
+ 
+ 
+ 
+ 
  
